d3733bfc2a080bac77046cdf836f2d72d9572076
chrobahs <47793145+chrobahs@users.noreply.github.com>
2019-10-30 22:48:44 -0500

    Create Second Endoint Assignment 6

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -25,6 +25,7 @@ namespace API.Controllers
       return this.context.Posts.ToList();
     }
       ///<summary>
+      /// GET api/post/[id]   
       ///</summary>
       ///<param name="id">Post id</param>
       ///<returns>A single post</returns>
@@ -32,6 +33,26 @@ namespace API.Controllers
       public ActionResult<Post> GetById(Guid id){
         return this.context.Posts.Find(id);
       }
-
+      ///<summary>
+      ///POST api/post
+      ///</summary>
+      ///<param name="request">JSON request containing post fields</param>
+      ///<returns>A new post</returns>
+      [HttpPost]
+      public ActionResult<Post> Create([FromBody]Post request)
+      {
+        var post = new Post{
+          Id=request.Id,
+          Title=request.Title,
+          Body=request.Body,
+          Date=request.Date
+        };
+        context.Posts.Add(post);
+        var success= context.SaveChanges()>0;
+        if (success){
+          return post;
+        }
+        throw new Exception("Error creating post");
+      }
   }
 }
